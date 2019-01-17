cb407337ea96a8d3e2e68b5a38944f43156f2cd0
xiaohuakang <huakangxiao@gmail.com>
2019-01-17 18:59:33 +0800

    创建会话

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+
+class SessionsController extends Controller
+{
+    /**
+     * 用户登录页
+     */
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    /**
+     * 验证用户提交信息
+     */
+    public function store(Request $request)
+    {
+        $credentials = $this->validate($request, [
+            'email' => 'required|email|max:255',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            session()->flash('success', '欢迎回来！');
+            return redirect()->route('users.show', [Auth::user()]);
+        } else {
+            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配！');
+            return redirect()->back()->withInput();
+        }
+    }
+
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,23 @@
 |
 */
 
+// 首页
 Route::get('/', 'StaticPagesController@home')->name('home');
+// 帮助页
 Route::get('/help', 'StaticPagesController@help')->name('help');
+// 关于页
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
+// 用户注册页
 Route::get('signup', 'UsersController@create')->name('signup');
 
+// 用户处理路由
 Route::resource('users', 'UsersController');
+
+
+// 用户登录页
+Route::get('login', 'SessionsController@create')->name('login');
+// 提交用户登录信息
+Route::post('login', 'SessionsController@store')->name('login');
+
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
