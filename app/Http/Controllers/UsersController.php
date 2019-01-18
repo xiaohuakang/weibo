71c34e8cd7d8530fcc542b072b09effb2bd07087
xiaohuakang <huakangxiao@gmail.com>
2019-01-18 11:13:49 +0800

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,16 +8,28 @@ use Auth;
 
 class UsersController extends Controller
 {
+    /**
+     * 用户页面注册页面
+     */
     public function create()
     {
         return view('users.create');
     }
 
+    /**
+     * 单个用户展示页
+     */
     public function show(User $user)
     {
         return view('users.show', compact('user'));
     }
 
+    /**
+     * 添加用户
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Validation\ValidationException
+     */
     public function store(Request $request)
     {
         $this->validate($request, [
@@ -38,5 +50,37 @@ class UsersController extends Controller
 
         return redirect()->route('users.show', [$user]);
     }
+
+    /**
+     * 用户页面
+     */
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    /**
+     * 更新用户信息
+     */
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+
+        $user->update($data);
+
+        session()->flash('success', '个人资料更新成功！');
+        return redirect()->route('users.show', $user->id);
+    }
+
+
 
 }
