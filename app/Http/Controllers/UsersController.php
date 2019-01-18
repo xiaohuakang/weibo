4733cfe9cfb690e65ce7231dc0dd3f98f454b173
xiaohuakang <huakangxiao@gmail.com>
2019-01-18 15:50:17 +0800

    查看用户列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,7 +11,7 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth', [
-            'except' => ['show', 'create', 'store']
+            'except' => ['show', 'create', 'store', 'index']
         ]);
 
         $this->middleware('guest', [
@@ -20,6 +20,14 @@ class UsersController extends Controller
 
     }
 
+    /**
+     * 用户列表页面
+     */
+    public function index()
+    {
+        $users = User::paginate(10);
+        return view('users.index', compact('users'));
+    }
 
     /**
      * 用户页面注册页面
