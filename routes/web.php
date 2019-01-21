041ef72faced1355e89acb0ef828542e263ddfc1
xiaohuakang <huakangxiao@gmail.com>
2019-01-21 16:51:16 +0800

    重置密码

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,20 @@ Route::delete('logout', 'SessionsController@destroy')->name('logout');
 
 // 激活邮箱
 Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
+
+/**
+ * 重置密码
+ */
+// 显示充值密码的邮箱发送页面
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+
+// 邮箱发送重设链接
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+
+// 密码更新页面
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+
+// 执行密码更新操作
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+
+
