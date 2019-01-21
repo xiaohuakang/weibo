7c1515538b6c5a02e1a27eb660538db33247d15f
xiaohuakang <huakangxiao@gmail.com>
2019-01-21 19:59:37 +0800

    移除邮件发送的 from 选项

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -124,13 +124,11 @@ class UsersController extends Controller
     {
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = 'xiaohuakang@163.com';
-        $name = 'xiaohuakang';
         $to   = $user->email;
         $subject = '感谢注册 Weibo App 应用，请确认您的邮箱！';
 
-        Mail::send($view, $data, function($message) use ($from, $name, $to, $subject) {
-           $message->from($from, $name)->to($to)->subject($subject);
+        Mail::send($view, $data, function($message) use ($to, $subject) {
+           $message->to($to)->subject($subject);
         });
     }
 
