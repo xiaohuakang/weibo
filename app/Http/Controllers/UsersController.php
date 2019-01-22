02464f1845e70c35552ceb323ad4d463d6f66f58
xiaohuakang <huakangxiao@gmail.com>
2019-01-22 17:32:31 +0800

    用户微博列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -43,7 +43,8 @@ class UsersController extends Controller
      */
     public function show(User $user)
     {
-        return view('users.show', compact('user'));
+        $statuses = $user->statuses()->orderBy('created_at', 'desc')->paginate(15);
+        return view('users.show', compact('user', 'statuses'));
     }
 
     /**
